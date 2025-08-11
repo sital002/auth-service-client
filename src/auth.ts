de82b2bd076f912aeab71b22f6f236b023e10c89
sital002 <sitaladhikari002@gmail.com>
2025-08-11 07:33:12 +0545

    signIn and signUp function

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
 import z from "zod";
 
 const SERVER_URL = "http://localhost:3000";
@@ -7,19 +7,29 @@ const loginResponse = z.object({
   access_token: z.string(),
 });
 
-export async function signIn({
-  email,
-  password,
-}: {
+type SignInInput = {
   email: string;
   password: string;
-}) {
-  const response = await axios.post(`${SERVER_URL}/user/signin`, {
-    email,
-    password,
-  });
-  const parsedData = loginResponse.parse(response.data);
-  return parsedData;
+};
+
+export async function signIn({ email, password }: SignInInput) {
+  try {
+    const response = await axios.post(`${SERVER_URL}/user/signin`, {
+      email,
+      password,
+    });
+    const parsedData = loginResponse.parse(response.data);
+    return parsedData;
+  } catch (err) {
+    if (isAxiosError(err)) {
+      throw new Error(err.response?.data);
+    }
+    if (err instanceof Error) {
+      throw err;
+    }
+
+    throw new Error("An unknown error occurred during sign in.");
+  }
 }
 
 export async function signUp({
